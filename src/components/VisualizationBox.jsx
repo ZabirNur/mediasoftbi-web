c3a2c2bd7f86bbb45559ec827e01a044585c24b2
Zabir Nur <zabir.mediasoft@gmail.com>
2021-12-07 18:29:42 +0600

    integrate recharts

--- src/components/VisualizationBox.jsx
+++ src/components/VisualizationBox.jsx
@@ -1,7 +1,41 @@
+import { LineChart, Line } from 'recharts'
+
+const data = [
+  {
+    name: "A",
+    revenue: 1000,
+    expense: 850
+  },
+  {
+    name: "B",
+    revenue: 1400,
+    expense: 660
+  },
+  {
+    name: "C",
+    revenue: 800,
+    expense: 1150
+  },
+  {
+    name: "D",
+    revenue: 1200,
+    expense: 1950
+  },
+  {
+    name: "E",
+    revenue: 650,
+    expense: 150
+  },
+]
+
+
 export default () => {
   return (
     <div>
-      
+      <LineChart width={200} height={200} data={data}>
+        <Line type="monotone" dataKey="revenue" stroke="green"/>
+        <Line type="monotone" dataKey="expense" stroke="red"/>
+      </LineChart>
     </div>
   )
 }
