6989603bb164fbce04a26e1e46a43f3cca20b72b
Zabir Nur <zabir.mediasoft@gmail.com>
2021-12-07 15:18:42 +0600

    set up header bar

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,18 +1,54 @@
 import styled from 'styled-components'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+
+const StyledProductName = styled.div`
+  font-size: 1.5rem;
+  user-select: none;
+`
+
 const ProductName = () => {
   return (
-    <div>
+    <StyledProductName>
       Mediasoft BI
-    </div>
+    </StyledProductName>
   )
 }
 
-const HeaderRightSide = ({children}) => {
+
+
+const StyledHeaderRightSide = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const StyledActionButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: 30px;
+`
+
+const StyledProfileButton = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 1px solid black;
+  border-radius: 50%;
+  background-color: paleturquoise;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  cursor: pointer;
+`
+
+const ProfileButton = () => {
   return (
-    <div>
-      {children}
-    </div>
+    <StyledProfileButton>
+      GD
+    </StyledProfileButton>
   )
 }
 
@@ -20,16 +56,22 @@ const StyledHeaderBar = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  height: 60px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-bottom: 1px solid silver;
 `
 
 export default () => {
   return (
     <StyledHeaderBar>
       <ProductName />
-      <HeaderRightSide>
-        <span>A</span>
-        <span>B</span>
-      </HeaderRightSide>
+      <StyledHeaderRightSide>
+        <ProfileButton/>
+        <StyledActionButtonContainer>
+          <FontAwesomeIcon icon={faEllipsisV} />
+        </StyledActionButtonContainer>
+      </StyledHeaderRightSide>
     </StyledHeaderBar>
   )
 }
